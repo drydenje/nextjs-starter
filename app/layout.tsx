2ad590aeb4da5154a57af785a343eb9c642f0571
Jeremy <jeremy.dryden@gmail.com>
2023-08-11 23:45:26 -0400

    need to tweak the folder layout still

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavBar from '@/src/navigation/components/NavBar'
+import NavBar from '@/src/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
