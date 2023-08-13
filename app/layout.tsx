ac5b69e9a7ffa2f78f577951a087d77fe75bae92
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-08-12 22:09:19 -0400

    added graphql function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavBar from '@/src/navigation'
+import NavBar from '@/src/navigation/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
