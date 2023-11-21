35b990cad3ee7427b05f07cfffc883dfe5853feb
Jeremy <jeremy.dryden@gmail.com>
2023-11-21 15:34:31 -0500

    cleaning up the nav bar, have some example links and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,12 @@
 import { Button } from '@/base/components/ui/button';
 // import FetchGraphQL from "@/src/components/"
 
-const test = process.env.SPACE_ID;
-console.log('test:', test);
-
 export default function Home() {
   return (
-    <main>
+    <>
       <Button>Click me, please</Button>
       <div>test in</div>
       <div>try without ui library</div>
-    </main>
+    </>
   );
 }
--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,19 +1,36 @@
+import Link from 'next/link';
+
 const NavBar = () => {
   return (
-    <nav role="navigation" aria-label="Site Navigation">
-      <ul>
-        <li>
-          <a href="#">One</a>
-        </li>
-        <li>
-          <a href="#">Two</a>
-        </li>
-        <li>
-          <a href="#">Three</a>
-        </li>
+    <nav
+      role="navigation"
+      aria-label="Site Navigation"
+      className="grid-in-nav flex flex-row 
+    items-center justify-end p-4 text-sm pr-0"
+    >
+      <ul
+        className="flex flex-row justify-around backdrop-blur-3xl 
+      tracking-widest items-center w-[55%] h-[60px]"
+      >
+        {[
+          ['00', 'Home', '/'],
+          ['01', 'Destination', '/destination'],
+          ['02', 'Crew', '/crew'],
+          ['03', 'Technology', '/technology'],
+        ].map(([num, title, url]) => (
+          <li
+            className="my-0 mx-0 uppercase font-sansCondensed text-base font-normal"
+            key={title}
+          >
+            <Link href={url}>
+              <span className="my-0 mx-2 font-bold">{num}</span>
+              {title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
