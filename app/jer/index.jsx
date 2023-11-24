7053f3ad6af1eae066a8007615296a3f45d3b7de
Jeremy <jeremy.dryden@gmail.com>
2023-11-23 21:52:19 -0500

    more menu done, seems to be ignoring routes in app folder now?

--- app/jer/index.jsx
+++ app/jer/index.jsx
@@ -1,3 +1,5 @@
+'use client';
+import React from 'react';
 // import { Button } from "@/components/ui";
 import { Button } from '@/base/components/ui/button';
 // import FetchGraphQL from "@/src/components/"
--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,6 +1,7 @@
-"use client"
+'use client';
 import * as React from 'react';
 import Link from 'next/link';
+import { cn } from '@/base/utils';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -10,30 +11,30 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-} from "@/base/components/ui/navigation-menu"
+} from '@/base/components/ui/navigation-menu';
 
-const todo: { title: string; href: string; description: string}[] = [
+const todo: { title: string; href: string; description: string }[] = [
   {
-    title: "Set the fonts",
-    href: "/todo#fonts",
-    description: "Download and set fonts for headings and body of text",
+    title: 'Set the fonts',
+    href: '/todo',
+    description: 'Download and set fonts for headings and body of text',
   },
   {
-    title: "Set the fonts",
-    href: "/todo#fonts",
-    description: "Download and set fonts for headings and body of text",
+    title: 'Set the fonts2',
+    href: '/todo#fonts',
+    description: 'Download and set fonts for headings and body of text',
   },
   {
-    title: "Set the fonts",
-    href: "/todo#fonts",
-    description: "Download and set fonts for headings and body of text",
+    title: 'Set the fonts3',
+    href: '/todo#fonts',
+    description: 'Download and set fonts for headings and body of text',
   },
   {
-    title: "Set the fonts",
-    href: "/todo#fonts",
-    description: "Download and set fonts for headings and body of text",
+    title: 'Set the fonts4',
+    href: '/todo#fonts',
+    description: 'Download and set fonts for headings and body of text',
   },
-]
+];
 
 const NavBar = () => {
   return (
@@ -43,17 +44,13 @@ const NavBar = () => {
           <NavigationMenuTrigger>Getting Started</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              {todo.map((item)=>(
-                <li
-                  key={item.title}
-                  title={item.title}
-                  href={item.href}
-                  >
-                    {item.description}
-                </li>
+              {todo.map((item) => (
+                <ListItem key={item.title} title={item.title} href={item.href}>
+                  {item.description}
+                </ListItem>
               ))}
 
-                {/* <NavigationMenuLink asChild>Link</NavigationMenuLink> */}
+              {/* <NavigationMenuLink asChild>Link</NavigationMenuLink> */}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
@@ -69,3 +66,30 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+
+ListItem.displayName = 'ListItem';
